0955d26b50ea01472cd0a7925f34002c8bf3f96f
Pranav K <prkrishn@hotmail.com>
2014-09-01 23:04:49 -0700

    Not Null

--- samples/HelloWorld/Compiler/Preprocess/HelloMetaProgramming.cs
+++ samples/HelloWorld/Compiler/Preprocess/HelloMetaProgramming.cs
@@ -1,14 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.Framework.Runtime;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.IO;
 
 namespace HelloWorld.Compiler.Preprocess
 {
+    class FooRewriter : CSharpSyntaxRewriter
+    {
+        public override SyntaxNode VisitMethodDeclaration(MethodDeclarationSyntax node)
+        {
+            var index = 0;
+            var statements = node.Body.Statements;
+            foreach (var parameter in node.ParameterList.Parameters)
+            {
+                var isNotNull = parameter.AttributeLists.SelectMany(l => l.Attributes)
+                                                        .Any(a => ((IdentifierNameSyntax)a.Name).Identifier.Value.Equals("NotNull"));
+                if (isNotNull)
+                {
+                    var ifStatement = SyntaxFactory.ParseStatement(string.Format(
+@"#line hidden
+if ({0} == null) {{ throw new {1}(nameof({0})); }}
+", parameter.Identifier, typeof(ArgumentNullException).FullName));
+                    if (index == 0)
+                    {
+                        var lineDefault = SyntaxFactory.ParseLeadingTrivia(@"#line default
+");
+                        var statement = statements[0].WithLeadingTrivia(lineDefault);
+                        statements = statements.Replace(statements[0], statement);
+                    }
+
+                    statements = statements.Insert(index++, ifStatement);
+                }
+            }
+
+            if (index > 0)
+            {
+                node = node.WithBody(node.Body.WithStatements(statements));
+            }
+
+            return base.VisitMethodDeclaration(node);
+        }
+    }
+
     public class HelloMetaProgramming : ICompileModule
     {
         public HelloMetaProgramming(IServiceProvider services)
@@ -17,14 +57,26 @@ namespace HelloWorld.Compiler.Preprocess
 
         public void BeforeCompile(IBeforeCompileContext context)
         {
-            context.CSharpCompilation = context.CSharpCompilation.AddSyntaxTrees(CSharpSyntaxTree.ParseText(@"
-public class Foo 
-{
-    public string Message
-    {
-        get { return ""Metaprogrammg!""; } 
-    }
-}"));
+            var rewriter = new FooRewriter();
+            var syntraxTrees = context.CSharpCompilation.SyntaxTrees;
+            foreach (var item in syntraxTrees)
+            {
+                var replaced = rewriter.Visit(item.GetRoot());
+                var blah = item.WithRootAndOptions(replaced, item.Options);
+                context.CSharpCompilation = context.CSharpCompilation.ReplaceSyntaxTree(item,
+                                                                                        blah);
+
+                if (!string.IsNullOrEmpty(item.FilePath))
+                {
+                    using (var writer = File.CreateText(@"D:\temp\ConsoleApp2\ConsoleApp2\" + System.IO.Path.GetFileName(item.FilePath)))
+                    {
+                        blah.GetText().Write(writer);
+                    }
+                }
+
+            }
+
+
         }
 
         public void AfterCompile(IAfterCompileContext context)
@@ -70,3 +122,5 @@ namespace Microsoft.Framework.Runtime
         IList<Diagnostic> Diagnostics { get; }
     }
 }
+
+
--- samples/HelloWorld/Program.cs
+++ samples/HelloWorld/Program.cs
@@ -1,24 +1,24 @@
+using System;
 using System.Reflection;
 
 public class Program
 {
     public int Main(string[] args)
     {
-        System.Console.WriteLine("Hello World!");
-
-        var resourceStream = typeof(Program).GetTypeInfo().Assembly.GetManifestResourceStream("compiler/resources/HTMLPage1.html");
-
-        if (resourceStream == null)
-        {
-            return 1;
-        }
-        
-        System.Console.WriteLine(HelloShared.HelloSharedCode.SharedMethod());
-        foreach (var arg in args)
-        {
-            System.Console.WriteLine(arg);
-        }
-
+        Bar("Hello World!");
+        Bar(null);
+        Console.ReadKey();
         return 0;
+    }
+
+
+    public static void Bar([NotNull] string x)
+    {
+        System.Console.WriteLine(x.Length);
+    }
+
+    public class NotNull : System.Attribute
+    {
+
     }
 }
