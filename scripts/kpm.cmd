8bd2341e79edf32c61b279c69ac7273b6a22904a
shhsu <shhsu@microsoft.com>
2015-01-02 10:47:48 -0800

    kpm --native is broken without Microsoft.Framework.Project

--- scripts/kpm.cmd
+++ scripts/kpm.cmd
@@ -2,7 +2,7 @@
 SETLOCAL
 SET ERRORLEVEL=
 
-"%~dp0klr" --appbase "%CD%" %K_OPTIONS% --lib "%~dp0lib\Microsoft.Framework.PackageManager" Microsoft.Framework.PackageManager %*
+"%~dp0klr" --appbase "%CD%" %K_OPTIONS% --lib "%~dp0lib\Microsoft.Framework.PackageManager;%~dp0lib\Microsoft.Framework.Project" Microsoft.Framework.PackageManager %*
 
 exit /b %ERRORLEVEL%
 ENDLOCAL
